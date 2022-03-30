2b1d530abe24213421bfc0ea9f2eb51d1333b206
Ferruh <ferruhcihan20@gmail.com>
2022-03-30 22:24:30 +0300

    feat:add Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Opensea Blockchain Clone</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Header />
 
       <h1 className="text-2xl font-bold">Opensea Blockchain Clone</h1>
     </div>
