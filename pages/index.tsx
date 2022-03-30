3b22c764b45ea8eec76e65ffd6bc4861195261e7
Ferruh <ferruhcihan20@gmail.com>
2022-03-30 22:31:58 +0300

    feat:update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,8 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col">
       <Header />
-
       <h1 className="text-2xl font-bold">Opensea Blockchain Clone</h1>
     </div>
   )
