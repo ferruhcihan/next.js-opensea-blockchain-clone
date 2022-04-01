6874820a320cbd0a8322cba53195ea4425d5998e
Ferruh <ferruhcihan20@gmail.com>
2022-04-01 03:10:34 +0300

    feat:add sanity client file

---
+++ lib/sanityClient.ts
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient({
+    projectId: 'zquydn4o',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    token:
+        'skdjMFAfFB5cP6ISSIiehrWh8IbrzmHiz4RdIag3oTieX0K1zJp3nin7Vlj4XUFbXPovHjpKAvWxGsCohUCcJFBAcwSjDW4gqviPfKn8zzBH4SMxnTRvxBQixfZYx6j5MkgQNgE6BtPeAskYCVo9MN8t9BFQyEEygW7n4b29fRD62QbpMQyL',
+    useCdn: false,
+})
